80fb501ed40be9905bac3cae7a09eca37902fb75
NitishGupta404 <73057078+NitishGupta404@users.noreply.github.com>
2021-01-06 18:45:50 +0530

    Delete Example.py

--- Example.py
+++
@@ -1,3 +0,0 @@
-import pywhatkit
-
-pywhatkit.text_to_handwriting("Access IMDb's metadata for every movie, TV series and video game title as well as performers and creators, along with full lifetime box office grosses from IMDbPro's Box Office Mojo. Our API and bulk data access products help entertainment fans share their passion with the world, including IMDb's 1-10 star rating, a daily-computed average of votes from IMDb's global audience of over 200 million fans." ,rgb=[0,0,0])
